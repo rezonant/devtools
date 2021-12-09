7c3afe69139a2bc41ed643e0ead15a1fd209e537
William Lahti <wilahti@gmail.com>
2021-12-09 00:41:45 -0800

    about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,31 @@
 import { Component } from "@angular/core";
 
 @Component({
-    template: `ABOUT HERE`
+    template: `
+        <header>
+            <h1>All your dev tooling in one spot</h1>
+            <div>
+                <code>@rezonant/devtools</code> is the hacker's companion. 
+                <a href="https://github.com/rezonant/devtools">Open source</a>. 
+            </div>
+        </header>
+    `,
+    styles: [
+        `
+        header {
+            height: 10em;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+            h1 {
+                font-size: 40px;
+                font-weight: 100;
+            }
+        }
+        `
+    ]
 })
 export class AboutComponent {
 
