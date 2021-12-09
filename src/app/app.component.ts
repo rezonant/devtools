97d0082ba610802e7e3d4e552825265d7b131873
William Lahti <wilahti@gmail.com>
2021-12-09 00:56:37 -0800

    add logo to console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,19 @@ export class AppComponent {
   isHome = false;
 
   ngOnInit() {
+    console.info(
+      `%c                                                                 `, 
+      `
+        font-size: 300%; 
+        padding: 1em 0 0 0; 
+        background-image: rgb(39, 39, 39); 
+        background-repeat: no-repeat;
+        background-position: left;
+        background-size: contain;
+        color: #fff; 
+        background-image: url(${window.origin}/assets/logo.svg);`
+    );
+    
     this.router.events.subscribe(ev => {
       this.isHome = this.router.url === '/';
     });
