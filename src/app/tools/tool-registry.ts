63bae06b0964c0c31cab65f1666970b60d0f8dc3
William Lahti <wilahti@gmail.com>
2021-12-12 02:24:39 -0800

    sort tool list

--- src/app/tools/tool-registry.ts
+++ src/app/tools/tool-registry.ts
@@ -2,6 +2,12 @@ import { Type } from "@angular/core";
 import { ToolComponent } from "./tool-component";
 
 export class ToolRegistry {
-    constructor(readonly tools : Type<ToolComponent>[]) {
+    constructor(tools : Type<ToolComponent>[]) {
+        this._tools = tools.sort((a, b) => (a['label'] as string).localeCompare(b['label']));
+    }
+
+    private _tools : Type<ToolComponent>[];
+    get tools() {
+        return this._tools;
     }
 }
